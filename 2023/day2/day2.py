c9ec59c05fe72c2c8683a6ad047008447ed8070f
Redmaddic <59644658+Redmaddic@users.noreply.github.com>
2023-12-04 20:02:40 +0100

    day 2

---
+++ 2023/day2/day2.py
@@ -0,0 +1,27 @@
+with open("2023/day2/data.txt", "r") as f:
+    data = f.readlines()
+
+sum = 0
+
+for line in data:
+    id = line.split(":")[0].strip("Game ")
+    red = 0
+    blue = 0
+    green = 0
+    grabs = line.strip("\n").split(": ")[1].split("; ")
+    for grab in grabs:
+        for entry in grab.split(", "):
+            entry = entry.split(" ")
+            number = entry[0]
+            color = entry[1]
+            if color == "red" and int(number) > red:
+                red = int(number)
+            elif color == "blue" and int(number) > blue:
+                blue = int(number)
+            elif color == "green" and int(number) > green:
+                green = int(number)
+    if red <= 12 and green <= 13 and blue <= 14:
+        sum += int(id)
+print(sum)
+
+    
