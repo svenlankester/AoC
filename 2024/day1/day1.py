13557f855eb8a2a2b1b8f2df3990b7219450af71
Redmaddic <59644658+Redmaddic@users.noreply.github.com>
2024-12-01 14:40:15 +0100

    day 1 fully oneliner-concoctioned

--- 2024/day1/day1.py
+++ 2024/day1/day1.py
@@ -1,3 +1,1 @@
 print(sum(map(lambda x, y: abs(x - y), sorted([*map(int, open('data.txt').read().split())][0::2]), sorted([*map(int, open('data.txt').read().split())][1::2]))))
-    
-
--- 2024/day1/day1_part2.py
+++ 2024/day1/day1_part2.py
@@ -1,11 +1,1 @@
-with open('data.txt', 'r') as f:
-    data = f.read().split()
-
-list1 = sorted([*map(int, data[0::2])])
-list2 = sorted([*map(int, data[1::2])])
-
-res = 0
-for item in list1:
-    res += (list2.count(item) * item)
-
-print(res)
+print(sum([a * sorted([*map(int, open('data.txt').read().split())][1::2]).count(a) for a in sorted([*map(int, open('data.txt').read().split())][0::2])]))
