5c6b1ce1522e54c32a8b223bba27ea4b6e5076d0
Redmaddic <59644658+Redmaddic@users.noreply.github.com>
2024-12-14 01:37:36 +0100

    day 13 fix

--- 2024/day13/day13_part2.py
+++ 2024/day13/day13_part2.py
@@ -9,11 +9,16 @@ for entry in data:
     machines.append(((int(entry[2][2:-1]), int(entry[3][2:])), (int(entry[6][2:-1]), int(entry[7][2:])), (int(entry[9][2:-1]) + 10000000000000, int(entry[10][2:]) + 10000000000000)))
 
 def get_minimal_cost(machine):
-    a, b, end = machine
-    input = np.array([[a[0], b[0]], [a[1], b[1]]])
-    output = np.array([end[0], end[1]])
-    res = np.linalg.solve(input, output)
-    
-    return int(3 * res[0] + res[1]) if (int(res[0]) * a[0] + int(res[1]) * b[0], int(res[0]) * a[1] + int(res[1]) * b[1]) == end else 0
+    a, b, output = machine
+    input = [[a[0], b[0]], [a[1], b[1]]]
+    determinant_orig = input[0][0] * input[1][1] - input[0][1] * input[1][0]
+    determinant_x = output[0] * input[1][1] - output[1] * input[0][1]
+    determinant_y = output[1] * input[0][0] - output[0] * input[1][0]
+
+    num_a = determinant_x/determinant_orig if determinant_x % determinant_orig  == 0 else 0
+    num_b = determinant_y/determinant_orig if determinant_y % determinant_orig  == 0 else 0
+
+    return int(3 * num_a + num_b)
+
 
 print(sum(map(get_minimal_cost, machines)))
