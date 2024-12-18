a66722844fe6e830e19269d7da4ecbc48646b12a
Redmaddic <59644658+Redmaddic@users.noreply.github.com>
2024-12-19 00:44:05 +0100

    optimized day 18 part 2

--- 2024/day18/day18_part2.py
+++ 2024/day18/day18_part2.py
@@ -19,19 +19,6 @@ def get_valid_moves(pos):
             moves.append(new_pos)
     return moves
 
-def check_dijkstra(pos):
-    dirs = [(0,1), (1,0), (0,-1), (-1,0), (1,1), (-1, -1), (1,-1), (-1,1)]
-    corrupt_count = 0
-    wall_count = 0
-    for dir in dirs:
-        new_pos = add_tuple(pos, dir)
-        if new_pos in corrupted:
-            corrupt_count += 1
-        elif not (0 <= new_pos[0] < size) or not (0 <= new_pos[1] < size):
-            wall_count += 1
-    return (corrupt_count > 1) or (wall_count > 0 and corrupt_count > 0)
-
-
 for i in range(1024):
     corrupted.add(data[i])
 
@@ -40,6 +27,7 @@ def dijkstra():
         for x in range(size):
             dists[(x,y)] = float('inf')
     dists[start] = 0
+    prev = {}
 
     prio_queue = [(0, start)]
     while prio_queue:
@@ -49,19 +37,33 @@ def dijkstra():
             continue
 
         if source == end:
-            return dist
+            return dist, prev
 
         for neighbour in get_valid_moves(source):
             alt = dists[source] + 1
             if alt < dists[neighbour]:
                 dists[neighbour] = alt
+                prev[neighbour] = source
                 heappush(prio_queue, (alt, neighbour))
-    return(dists[end])
+    return dists[end], prev
+
+_, prevs = dijkstra()
+path = set()
+curr = end
+while curr != start:
+    path.add(curr)
+    curr = prevs[curr]
 
 for i in range(1024,len(data)):
     new_corrupt = data[i]
     corrupted.add(new_corrupt)
-    if check_dijkstra(new_corrupt):
-        if dijkstra() == float('inf'):
+    if new_corrupt in path:
+        dist, prevs = dijkstra()
+        if dist == float('inf'):
             print(new_corrupt)
             break
+        path = set()
+        curr = end
+        while curr != start:
+            path.add(curr)
+            curr = prevs[curr]
