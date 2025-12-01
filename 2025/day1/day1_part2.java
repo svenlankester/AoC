93bc86993c45f4e699cbb947cfe2bb97c798a216
svenlankester <svenlankester@gmail.com>
2025-12-01 21:17:04 +0100

    initial commit 2025 + day 1 part 1

---
+++ 2025/day1/day1_part2.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class day1_part2 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new FileReader("data.txt"));
+        String line;
+        while ((line = br.readLine()) != null) {
+            
+        }
+    }
+}
