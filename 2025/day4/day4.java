db40ed90cd320bacfc5bedca6b4e025e626ae65d
svenlankester <svenlankester@gmail.com>
2025-12-04 15:14:44 +0100

    Day 4 part 1

---
+++ 2025/day4/day4.java
@@ -0,0 +1,46 @@
+package day4;
+
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.util.*;
+import java.awt.Point;
+
+public class day4 {
+
+    static List<Point> adjacencyMatrix = new ArrayList<>(Arrays.asList(
+            new Point(-1, -1), new Point(0,-1), new Point (1, -1),
+            new Point(-1, 0),                       new Point (1, 0),
+            new Point(-1, 1), new Point(0,1), new Point (1, 1)
+    ));
+
+    static void main() throws IOException {
+        List<String> input = Files.readAllLines(new File("2025/day4/data.txt").toPath(), Charset.defaultCharset());
+        int total = 0;
+
+        for (int i = 0; i < input.size(); i++) {
+            for (int j = 0; j < input.getFirst().length(); j++) {
+                if (input.get(i).charAt(j) == '@') {
+                    if (isAccessible(input, j, i)) total++;
+                }
+            }
+        }
+
+        System.out.println(total);
+    }
+
+    static boolean isAccessible(List<String> input, int x, int y) {
+        int total = 0;
+
+        for (Point p : adjacencyMatrix) {
+            int newX = x + p.x;
+            int newY = y + p.y;
+
+            if (0 <= newY && newY < input.size() && 0 <= newX && newX < input.getFirst().length()) {
+                if (input.get(newY).charAt(newX) == '@') total++;
+            }
+        }
+
+        return total < 4;
+    }
+}
