c26821045246e3e4b03208ccfa18772c89bad058
svenlankester <svenlankester@gmail.com>
2025-12-11 20:45:23 +0100

    day 11 part 1

---
+++ 2025/day11/day11.java
@@ -0,0 +1,42 @@
+package day11;
+
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.util.*;
+import org.jgrapht.*;
+import org.jgrapht.alg.shortestpath.AllDirectedPaths;
+import org.jgrapht.graph.*;
+import org.jgrapht.nio.*;
+import org.jgrapht.nio.dot.*;
+import org.jgrapht.traverse.*;
+
+
+public class day11 {
+    static void main() throws IOException {
+        List<String> input = Files.readAllLines(new File("2025/day11/data.txt").toPath(), Charset.defaultCharset());
+        
+        HashMap<String, List<String>> connections = new LinkedHashMap<>();
+        
+        for (String line : input) {
+            String key = line.substring(0, 3);
+            List<String> val = List.of(line.substring(5).split(" "));
+            connections.put(key, val);
+        }
+        
+        Graph<String, DefaultEdge> g = new DefaultDirectedGraph<>(DefaultEdge.class);
+        
+        // first add all vertices
+        g.addVertex("out");
+        for (String s : connections.keySet()) g.addVertex(s);
+        
+        for (String key : connections.keySet()) {
+            for (String output : connections.get(key)) {
+                g.addEdge(key, output);
+            }
+        }
+
+        AllDirectedPaths<String, DefaultEdge> paths = new AllDirectedPaths<>(g);
+        System.out.println(paths.getAllPaths("you", "out", true, 9999).size());
+    }
+}
